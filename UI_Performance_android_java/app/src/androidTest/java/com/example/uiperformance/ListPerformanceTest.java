5d7f9d2c845f20cc64312778e995d60f23c728cf
Nazar Tsybulskyi <nazar.tsybulskyi@inveritasoft.com>
2020-03-27 18:40:13 +0200

    added delay to  android test

--- UI_Performance_android_java/app/src/androidTest/java/com/example/uiperformance/ListPerformanceTest.java
+++ UI_Performance_android_java/app/src/androidTest/java/com/example/uiperformance/ListPerformanceTest.java
@@ -2,14 +2,19 @@ package com.example.uiperformance;
 
 
 import android.content.Intent;
+import android.view.View;
 
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.filters.LargeTest;
 import androidx.test.runner.AndroidJUnit4;
 
+import org.hamcrest.Matcher;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,35 +25,19 @@ import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class ListPerformanceTest {
 
-//    private static final String TEXT_ITEM_30 = "item: 30";
-//
-//    private static final String TEXT_ITEM_30_SELECTED = "30";
-//
-//    private static final String TEXT_ITEM_60 = "item: 60";
-//
-//    // Match the last item by matching its text.
-//    private static final String LAST_ITEM_ID = "item: 99";
-   private static final int RV_ID = R.id.recyclerview;
-
-
     protected ActivityScenario<MainActivity> recyclerViewActivityScenario;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         recyclerViewActivityScenario = ActivityScenario.launch(MainActivity.class);
     }
-
-
-
-//    @Rule
-//    public ActivityScenarioRule<MainActivity> rule = new ActivityScenarioRule<>(
-//            MainActivity.class);
 
 
     @Test
@@ -57,8 +46,6 @@ public class ListPerformanceTest {
             forward();
             revert();
         }
-        //onView(withId(RV_ID)).perform(scrollTo(hasDescendant(withText(ITEM_64))));
-        //onView(withItemText(ITEM_64)).check(matches(isDisplayed()));
     }
 
     public void forward(){
@@ -66,42 +53,61 @@ public class ListPerformanceTest {
                 RecyclerViewActions.scrollToPosition(10)
                 //actionOnItemAtPosition(step, scrollTo())
         );
+        onView(isRoot()).perform(waitFor(600));
+
         onView(withId(R.id.recyclerview)).perform(
                 RecyclerViewActions.scrollToPosition(20)
                 //actionOnItemAtPosition(step, scrollTo())
         );
+        onView(isRoot()).perform(waitFor(600));
+
         onView(withId(R.id.recyclerview)).perform(
                 RecyclerViewActions.scrollToPosition(30)
                 //actionOnItemAtPosition(step, scrollTo())
         );
+        onView(isRoot()).perform(waitFor(600));
+
         onView(withId(R.id.recyclerview)).perform(
                 RecyclerViewActions.scrollToPosition(40)
                 //actionOnItemAtPosition(step, scrollTo())
         );
+        onView(isRoot()).perform(waitFor(600));
+
         onView(withId(R.id.recyclerview)).perform(
                 RecyclerViewActions.scrollToPosition(50)
                 //actionOnItemAtPosition(step, scrollTo())
         );
+        onView(isRoot()).perform(waitFor(600));
+
         onView(withId(R.id.recyclerview)).perform(
                 RecyclerViewActions.scrollToPosition(60)
                 //actionOnItemAtPosition(step, scrollTo())
         );
+        onView(isRoot()).perform(waitFor(600));
+
         onView(withId(R.id.recyclerview)).perform(
                 RecyclerViewActions.scrollToPosition(70)
                 //actionOnItemAtPosition(step, scrollTo())
         );
+        onView(isRoot()).perform(waitFor(600));
+
         onView(withId(R.id.recyclerview)).perform(
                 RecyclerViewActions.scrollToPosition(80)
                 //actionOnItemAtPosition(step, scrollTo())
         );
+        onView(isRoot()).perform(waitFor(600));
+
         onView(withId(R.id.recyclerview)).perform(
                 RecyclerViewActions.scrollToPosition(90)
                 //actionOnItemAtPosition(step, scrollTo())
         );
+        onView(isRoot()).perform(waitFor(600));
+
         onView(withId(R.id.recyclerview)).perform(
                 RecyclerViewActions.scrollToPosition(100)
                 //actionOnItemAtPosition(step, scrollTo())
         );
+        onView(isRoot()).perform(waitFor(600));
     }
 
     public void revert(){
@@ -109,101 +115,78 @@ public class ListPerformanceTest {
                 RecyclerViewActions.scrollToPosition(90)
                 //actionOnItemAtPosition(step, scrollTo())
         );
+        onView(isRoot()).perform(waitFor(600));
+
         onView(withId(R.id.recyclerview)).perform(
                 RecyclerViewActions.scrollToPosition(80)
                 //actionOnItemAtPosition(step, scrollTo())
         );
+        onView(isRoot()).perform(waitFor(600));
+
         onView(withId(R.id.recyclerview)).perform(
                 RecyclerViewActions.scrollToPosition(70)
                 //actionOnItemAtPosition(step, scrollTo())
         );
+        onView(isRoot()).perform(waitFor(600));
+
         onView(withId(R.id.recyclerview)).perform(
                 RecyclerViewActions.scrollToPosition(60)
                 //actionOnItemAtPosition(step, scrollTo())
         );
+        onView(isRoot()).perform(waitFor(600));
+
         onView(withId(R.id.recyclerview)).perform(
                 RecyclerViewActions.scrollToPosition(50)
                 //actionOnItemAtPosition(step, scrollTo())
         );
+        onView(isRoot()).perform(waitFor(600));
+
         onView(withId(R.id.recyclerview)).perform(
                 RecyclerViewActions.scrollToPosition(40)
                 //actionOnItemAtPosition(step, scrollTo())
         );
+        onView(isRoot()).perform(waitFor(600));
+
         onView(withId(R.id.recyclerview)).perform(
                 RecyclerViewActions.scrollToPosition(30)
                 //actionOnItemAtPosition(step, scrollTo())
         );
+        onView(isRoot()).perform(waitFor(600));
+
         onView(withId(R.id.recyclerview)).perform(
                 RecyclerViewActions.scrollToPosition(20)
                 //actionOnItemAtPosition(step, scrollTo())
         );
+        onView(isRoot()).perform(waitFor(600));
+
         onView(withId(R.id.recyclerview)).perform(
                 RecyclerViewActions.scrollToPosition(10)
                 //actionOnItemAtPosition(step, scrollTo())
         );
+        onView(isRoot()).perform(waitFor(600));
+
         onView(withId(R.id.recyclerview)).perform(
                 RecyclerViewActions.scrollToPosition(0)
                 //actionOnItemAtPosition(step, scrollTo())
         );
+        onView(isRoot()).perform(waitFor(600));
     }
 
-//    @Test
-//    public void testSample(){
-//        onView(withId(R.id.recyclerview)).perform(
-//                RecyclerViewActions.actionOnItemAtPosition(0, click())
-//        );
-//    }
 
-//    @Test
-//    public void lastItem_NotDisplayed() {
-//        // Last item should not exist if the list wasn't scrolled down.
-//        onView(withText(LAST_ITEM_ID)).check(doesNotExist());
-//    }
-//
-//
-//    @Test
-//    public void list_Scrolls() {
-//        onRow(LAST_ITEM_ID).check(matches(isCompletelyDisplayed()));
-//    }
+    public static ViewAction waitFor(final long delay) {
+        return new ViewAction() {
+            @Override public Matcher<View> getConstraints() {
+                return isRoot();
+            }
 
+            @Override public String getDescription() {
+                return "wait for " + delay + "milliseconds";
+            }
 
-//    @Test
-//    public void row_Click() {
-//        // Click on one of the rows.
-//        onRow(TEXT_ITEM_30).onChildView(withId(R.id.rowContentTextView)).perform(click());
-//
-//        // Check that the activity detected the click on the first column.
-//        onView(ViewMatchers.withId(R.id.selection_row_value))
-//                .check(matches(withText(TEXT_ITEM_30_SELECTED)));
-//    }
-//
-//
-//    @Test
-//    public void toggle_Click() {
-//        // Click on a toggle button.
-//        onRow(TEXT_ITEM_30).onChildView(withId(R.id.rowToggleButton)).perform(click());
-//
-//        // Check that the toggle button is checked.
-//        onRow(TEXT_ITEM_30).onChildView(withId(R.id.rowToggleButton)).check(matches(isChecked()));
-//    }
-//
-//
-//    @Test
-//    public void toggle_ClickDoesntPropagate() {
-//        // Click on one of the rows.
-//        onRow(TEXT_ITEM_30).onChildView(withId(R.id.rowContentTextView)).perform(click());
-//
-//        // Click on the toggle button, in a different row.
-//        onRow(TEXT_ITEM_60).onChildView(withId(R.id.rowToggleButton)).perform(click());
-//
-//        // Check that the activity didn't detect the click on the first column.
-//        onView(ViewMatchers.withId(R.id.selection_row_value))
-//                .check(matches(withText(TEXT_ITEM_30_SELECTED)));
-//    }
-//
-//    private static DataInteraction onRow(String str) {
-//        return onData(hasEntry(equalTo(MainActivity.ROW_TEXT), is(str)));
-//    }
-
+            @Override public void perform(UiController uiController, View view) {
+                uiController.loopMainThreadForAtLeast(delay);
+            }
+        };
+    }
 
 }
